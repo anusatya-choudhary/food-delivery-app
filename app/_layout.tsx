b150e43d611bc4579feaeb8aced4905f4128f42a
Anusatya <anusatya.choudhary@gmail.com>
2025-08-20 02:07:56 +0530

    Implement custom font loading and splash screen handling in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "../global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontsError] = useFonts({
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsError) throw fontsError;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, fontsError]);
+ 
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Edit app/index.tsx to edit this screen</Text>
+      <Text className="text-4xl font-bold text-primary font-quicksand-bold">Welcome to NativeWind</Text>
     </View>
   );
 }
