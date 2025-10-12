054f2c93d319c9ddbfc87a15dee39de914965a25
Anusatya <anusatya.choudhary@gmail.com>
2025-10-12 17:22:15 +0530

    Enhance global styles with new utility and component classes, add clsx dependency for conditional styling, and update HomeScreen to utilize new styles and images for offers display.

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,41 @@
-import { offers, type Offer } from "@/constants";
-import { FlatList, Pressable, Text, View } from "react-native";
+import { images, offers, type Offer } from "@/constants";
+import clsx from "clsx";
+import { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="flex-1 bg-white">
       <FlatList
         data={offers}
         keyExtractor={(item: Offer) => item.id.toString()}
-        renderItem={( {item} : { item: Offer }) => {
+        renderItem={({ item, index }: { item: Offer, index: number }) => {
+          const isEven = index % 2 === 0;
           return <View>
-            <Pressable className="bg-amber-600">
-              <Text>{item.title}</Text>
+            <Pressable
+              className={clsx("offer-card", isEven ? "flex-row-reverse" : "flex-row")}
+              style={{ backgroundColor: item.color }}>
+              {({ pressed }) => (
+                <Fragment>
+                  <View className={"h-full w-1/2"}>
+                    <Image source={item.image} className="size-full" resizeMode="contain" />
+                  </View>
+                  <View className={clsx("offer-card__info", isEven ? "pl-10" : "pr-10")}>
+                    <Text className={"h1-bold text-white leading-tight"}>{item.title}</Text>
+                    <Image
+                      source={images.arrowRight}
+                      className="size-10"
+                      resizeMode="contain"
+                      tintColor="#ffffff"
+                    />
+                  </View>
+                </Fragment>
+              )}
             </Pressable>
           </View>
         }}
+        contentContainerClassName="pb-28 px-5"
       />
     </SafeAreaView>
   );
