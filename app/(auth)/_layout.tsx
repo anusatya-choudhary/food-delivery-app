1886a61f475f218dd95ae0a5cd4e15580a24f6cf
Anusatya <anusatya.choudhary@gmail.com>
2026-01-25 19:30:13 +0530

    Refactor authentication layout to include a keyboard avoiding view, scrollable content, and background images for improved user experience.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,12 +1,20 @@
+import { images } from "@/constants";
 import { Slot } from "expo-router";
 import React from "react";
-import {  Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView, View, Dimensions, ImageBackground, Image } from "react-native";
 
 export default function _Layout() {
+  const LOGIN_GRAPHIC_HEIGHT = Dimensions.get('screen').height / 2.25;
+  
   return (
-    <SafeAreaView>
-      <Text>Auth Layout</Text>
-      <Slot/>
-    </SafeAreaView>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+        <View className="w-full relative" style={{ height: LOGIN_GRAPHIC_HEIGHT }}>
+          <ImageBackground source={images.loginGraphic} className="size-full rounded-b-lg" resizeMode="stretch" />
+       <Image source={images.logo} className="self-center size-48 absolute -bottom-16" />
+        </View>
+      </ScrollView>
+      <Slot />
+    </KeyboardAvoidingView>
   );
 }
