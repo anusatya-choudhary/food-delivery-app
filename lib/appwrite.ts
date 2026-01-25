32cc674f071a2353fae06849fc5db2af743fa522
Anusatya <anusatya.choudhary@gmail.com>
2026-01-25 17:05:23 +0530

    Add databaseId to appwriteSetup for enhanced configuration

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,6 @@
 export const appwriteSetup = {
     endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform : "com.anusatya-choudhary.food-delivery-app",
-    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId : process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID
 }
